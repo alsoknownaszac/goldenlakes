a6954c438161ff48dcbae11b4fee525afa6c6947
alsoknownaszac <mayo16collins@gmail.com>
2024-10-15 22:13:01 +0100

    adding webhooks

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  },
   images: {
     remotePatterns: [
       {
